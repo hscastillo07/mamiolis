ad7625a1116183e53e648e13932a8fbe207f9825
hscastillo07 <hscastillo07@gmail.com>
2026-03-10 14:54:13 -0500

    fix merge conflicts

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
+import { PageShell } from "@/app/layout/PageShell";
 import { Providers } from "@/app/providers";
 import { HomePage } from "@/app/routes/HomePage";
 
 export default function App() {
   return (
     <Providers>
-      <HomePage />
+      <PageShell>
+        <HomePage />
+      </PageShell>
     </Providers>
   );
 }
--- src/app/routes/HomePage.tsx
+++ src/app/routes/HomePage.tsx
@@ -1,11 +1,3 @@
-/*import { HeroSection } from "@/components/sections/HeroSection";
-import { StorySection } from "@/components/sections/StorySection";
-import { QuotesSection } from "@/components/sections/QuotesSection";
-import { GallerySection } from "@/components/sections/GallerySection";
-import { ExperiencesSection } from "@/components/sections/ExperiencesSection";
-import { QrStorySection } from "@/components/sections/QrStorySection";
-import { MenuSection } from "@/components/sections/MenuSection";
-import { LocationHoursSection } from "@/components/sections/LocationHoursSection";*/
 
 import { Community } from "@/components/sections/figma/Community";
 import { Experiences } from "@/components/sections/figma/Experiences";
@@ -18,14 +10,6 @@ import { Story } from "@/components/sections/figma/Story";
 export function HomePage() {
   return (
     <>
-      {/* <HeroSection /> 
-      <StorySection />
-      <QuotesSection />
-      <GallerySection />
-      <ExperiencesSection />
-      <QrStorySection />
-      <MenuSection />
-      <LocationHoursSection />*/}
 
       <Hero />
       <Story />
